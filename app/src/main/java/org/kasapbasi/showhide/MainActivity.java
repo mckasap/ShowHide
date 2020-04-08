3ecff9792ec70b8970a74b4dc2422df8d19cc723
mckasap <mckasap@gmail.com>
2020-04-08 18:43:40 +0300

    UI is ready code added

--- app/src/main/java/org/kasapbasi/showhide/MainActivity.java
+++ app/src/main/java/org/kasapbasi/showhide/MainActivity.java
@@ -3,14 +3,26 @@ package org.kasapbasi.showhide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+ TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.theTv);
+
     }
 
-    
+    public void show(View v){
+tv.setVisibility(View.VISIBLE);
+
+    }
+    public void hide(View v){
+
+        tv.setVisibility(View.INVISIBLE);
+    }
+
 }
